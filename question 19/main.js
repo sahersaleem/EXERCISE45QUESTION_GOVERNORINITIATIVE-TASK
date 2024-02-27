9346507e1c80369fff861d7163c101bde9b86fa3
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:13:29 +0500

    Delete question 19 directory

--- question 19/main.js
+++
@@ -1,5 +0,0 @@
-var guestList = ["john", "Evie", "Austin"];
-console.log("hii " + guestList[0] + " i would like to invite you at dinner in my home");
-console.log("hii " + guestList[1] + " i would like to invite at you dinner in my home");
-console.log("hii " + guestList[2] + " i would like to invite at  you dinner in my home");
-console.log("I invited " + guestList.length + " for dinner in my house");
