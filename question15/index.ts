bc926e99ce2f833982dc91328dab67cad84fae2d
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:15:07 +0500

    Delete question15 directory

--- question15/index.ts
+++
@@ -1,7 +0,0 @@
-let guestList = ["john","Evie","Austin"];
-console.log("hii "+guestList[0]+ " i would like to invite you at dinner in my home")
-console.log("hii "+guestList[1]+ " i would like to invite at you dinner in my home")
-console.log("hii "+guestList[2]+ " i would like to invite at  you dinner in my home")
-console.log(guestList[0]);
-guestList[0]="Haris";
-console.log("hii "+guestList[0]+ " i would like to invite you at dinner in my home")
