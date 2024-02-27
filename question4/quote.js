3eb674efe8c983156777975859bc9dbd13e28904
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:15:32 +0500

    Delete question4 directory

--- question4/quote.js
+++
@@ -1,2 +0,0 @@
-var quote = 'Albert Einstein once said,"A person who never made a mistake never tried anything new."';
-console.log(quote);
--- question4/quote.ts
+++
@@ -1,2 +0,0 @@
-let quote = 'Albert Einstein once said,"A person who never made a mistake never tried anything new."';
-console.log(quote);
