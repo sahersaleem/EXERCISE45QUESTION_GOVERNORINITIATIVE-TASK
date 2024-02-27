596498e4b352ee2483bf868dcab99ce31a052485
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:13:51 +0500

    Delete question 3 directory

--- question 3/main.js
+++
@@ -1,7 +0,0 @@
-var firstName = "SaHeR";
-var lowerCaseString = firstName.toLowerCase();
-console.log(lowerCaseString);
-var toUpperCaseString = firstName.toUpperCase();
-console.log(toUpperCaseString);
-//let titleCasedString = firstName.toTitleCase();
-//console.log(titleCasedString);
--- question 3/main.ts
+++
@@ -1,7 +0,0 @@
-let firstName = "SaHeR";
-let lowerCaseString = firstName.toLowerCase();
-console.log(lowerCaseString);
-let toUpperCaseString = firstName.toUpperCase();
-console.log(toUpperCaseString);
-//let titleCasedString = firstName.toTitleCase();
-//console.log(titleCasedString);
