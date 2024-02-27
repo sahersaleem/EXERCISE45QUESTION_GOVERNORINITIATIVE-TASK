c711c67e2a3285acb87f563a5f05d77a91a28ed1
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:14:43 +0500

    Delete question13 directory

--- question13/array.ts
+++
@@ -1,4 +0,0 @@
-let transport = ["car","bikes","motorcycles"];
-console.log("when i will become software engineer , i wiil buy my favourite " +transport[0]+ " ,its my dream ");
-console.log("The "+transport[1]+ " are affordable for everyone");
-console.log("these buses are so beautiful " +transport[2]+ " and comfortable");
