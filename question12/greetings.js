25e7a4345ff4aa2cdbffbcb77049c476f53c0988
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:14:32 +0500

    Delete question12 directory

--- question12/greetings.js
+++
@@ -1,4 +0,0 @@
-var friendsName = ["AYESHA", "MOMINA", "MAAZ"];
-console.log("hii " + friendsName[0] + " how are you doing?");
-console.log("hii " + friendsName[1] + " how was your weekend?");
-console.log("hello , how are you doing today " + friendsName[2] + " i am good!");
--- question12/greetings.ts
+++
@@ -1,4 +0,0 @@
-let friendsName = ["AYESHA","MOMINA","MAAZ"];
-console.log("hii " +friendsName[0]+ " how are you doing?" );
-console.log("hii "+friendsName[1]+" how was your weekend?");
-console.log ("hello , how are you doing today " +friendsName[2]+ " i am good!")
