75bd1969c683a0bb25f652429a1381444e79b67d
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:14:08 +0500

    Delete question 2 directory

--- question 2/main.js
+++
@@ -1,3 +0,0 @@
-var studentsName = "Saher Saleem";
-var aMessage = "hello ".concat(studentsName, ", would you like to learn some python today");
-console.log(aMessage);
--- question 2/main.ts
+++
@@ -1,3 +0,0 @@
-let studentsName:String = "Saher Saleem"
-let aMessage : String = `hello ${studentsName}, would you like to learn some python today`
-console.log(aMessage)
