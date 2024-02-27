7b20e6ae496b3ab1f2a568901e35a57481abe5d4
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:15:53 +0500

    Delete question6 directory

--- question6/strip.js
+++
@@ -1,4 +0,0 @@
-var originalName = "\t\nsaher\tsaleem\t\n";
-var strippedName = (originalName.trim());
-console.log(strippedName);
-console.log(originalName);
--- question6/strip.ts
+++
@@ -1,4 +0,0 @@
-let originalName:string="\t\nsaher\tsaleem\t\n";
-let strippedName: string= (originalName.trim());
-console.log(strippedName);
-console.log(originalName);
