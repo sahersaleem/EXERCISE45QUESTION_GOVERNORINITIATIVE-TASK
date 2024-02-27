b48139a86ba1789b028f2dc07bcba7517b426ad4
sahersaleem <160317082+sahersaleem@users.noreply.github.com>
2024-02-27 07:15:42 +0500

    Delete question5 directory

--- question5/famousquote2.js
+++
@@ -1,4 +0,0 @@
-var famous_person = "ALBERT EINSTEIN";
-var message = 'once said, "A person who never made a mistake nrver tried anything new" ';
-console.log(famous_person);
-console.log(message);
--- question5/famousquote2.ts
+++
@@ -1,4 +0,0 @@
-let famous_person = "ALBERT EINSTEIN";
-let message = 'once said, "A person who never made a mistake nrver tried anything new" '
-console.log(famous_person);
-console.log(message);
